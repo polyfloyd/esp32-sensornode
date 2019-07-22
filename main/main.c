91c6d97518798391ec83a2d55df0201a20d64283
polyfloyd <floyd@polyfloyd.net>
2019-07-22 22:58:49 +0200

    Fix handling of IDF downgrades

--- main/main.c
+++ main/main.c
@@ -13,8 +13,12 @@ void geiger_cb() {
 }
 
 void app_main(void) {
-    nvs_flash_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = ESP_OK;
+    }
+    ESP_ERROR_CHECK(err);
 
     const i2c_config_t i2c_conf = {
         .mode             = I2C_MODE_MASTER,
