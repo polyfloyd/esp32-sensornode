cc609dd9967e7626c10d02ad8822fbb5f214e33b
polyfloyd <floyd@polyfloyd.net>
2019-08-03 16:40:54 +0200

    geiger: Disable internal pullup

--- main/geiger.c
+++ main/geiger.c
@@ -30,7 +30,7 @@ esp_err_t geiger_init(gpio_num_t int_pin, void (*callback)()) {
         .pin_bit_mask = 1<<int_pin,
         .mode         = GPIO_MODE_INPUT,
         .pull_up_en   = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type    = GPIO_INTR_POSEDGE,
     };
     TRY(gpio_config(&io_conf));
