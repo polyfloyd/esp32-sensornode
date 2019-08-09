2fa2cfba5ea9884a30878e5c1c7ea0d6c49f3489
polyfloyd <floyd@polyfloyd.net>
2019-08-09 23:01:36 +0200

    prometheus_esp32: Remove background task

--- main/prometheus_esp32.c
+++ main/prometheus_esp32.c
@@ -1,47 +1,74 @@
 #include "prometheus_esp32.h"
 #include <esp_event_loop.h>
 #include <esp_heap_caps.h>
+#include <string.h>
 #include "prometheus.h"
 
 
-prom_gauge_t _metric_heap_info;
-prom_gauge_t _metric_task_num;
+size_t mem_num_metrics(void *ctx) {
+    return 2;
+}
 
-void _esp32_metrics_task(void *pvParameters) {
-    while (true) {
-        multi_heap_info_t heap_info;
-        heap_caps_get_info(&heap_info, MALLOC_CAP_DEFAULT);
-        prom_gauge_set(&_metric_heap_info, heap_info.total_free_bytes, "free");
-        prom_gauge_set(&_metric_heap_info, heap_info.total_allocated_bytes, "alloc");
+void mem_collect(void *ctx, prom_metric_t *metrics, size_t num_metrics) {
+    assert(num_metrics == 2);
 
-        int num_tasks = uxTaskGetNumberOfTasks();
-        prom_gauge_set(&_metric_task_num, num_tasks);
+    multi_heap_info_t heap_info;
+    heap_caps_get_info(&heap_info, MALLOC_CAP_DEFAULT);
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+    prom_metric_t *m_free = &metrics[0];
+    m_free->value = heap_info.total_free_bytes;
+    strncpy(m_free->label_values, "free", PROM_MAX_LABEL_VALUES_LENGTH);
+
+    prom_metric_t *m_alloc = &metrics[1];
+    m_alloc->value = heap_info.total_allocated_bytes;
+    strncpy(m_alloc->label_values, "alloc", PROM_MAX_LABEL_VALUES_LENGTH);
+}
+
+size_t num_tasks_num_metrics(void *ctx) {
+    return 1;
+}
+
+void num_tasks_collect(void *ctx, prom_metric_t *metrics, size_t num_metrics) {
+    assert(num_metrics == 1);
+
+    prom_metric_t *m = &metrics[0];
+    m->value = uxTaskGetNumberOfTasks();
+    m->label_values[0] = 0;
 }
 
 void init_metrics_esp32(prom_registry_t *registry) {
-    prom_strings_t mem_strings = {
+    static prom_strings_t mem_strings = {
         .namespace = NULL,
         .subsystem = "esp32",
         .name      = "heap_bytes",
         .help      = "Usage of the system's heap in bytes",
     };
-    const char *mem_labels[] = { "type" };
-    prom_gauge_init_vec(&_metric_heap_info, mem_strings, mem_labels, 1);
-    prom_register_gauge(registry, &_metric_heap_info);
+    static const char *mem_labels[] = { "type" };
+    prom_collector_t mem_col = {
+        .strings     = &mem_strings,
+        .num_labels  = 1,
+        .labels      = mem_labels,
+        .type        = PROM_TYPE_GAUGE,
+        .ctx         = NULL,
+        .num_metrics = mem_num_metrics,
+        .collect     = mem_collect,
+    };
+    prom_register(registry, mem_col);
 
-    prom_strings_t task_num_strings = {
+    static prom_strings_t num_tasks_strings = {
         .namespace = NULL,
         .subsystem = "esp32",
         .name      = "task_num",
         .help      = "The current number of tasks",
     };
-    prom_gauge_init(&_metric_task_num, task_num_strings);
-    prom_register_gauge(registry, &_metric_task_num);
-
-    TaskHandle_t h = NULL;
-    xTaskCreate(_esp32_metrics_task, "prometheus-system", 2048, NULL, tskIDLE_PRIORITY, &h);
-    assert(h);
+    prom_collector_t num_tasks_col = {
+        .strings     = &num_tasks_strings,
+        .num_labels  = 0,
+        .labels      = NULL,
+        .type        = PROM_TYPE_GAUGE,
+        .ctx         = NULL,
+        .num_metrics = num_tasks_num_metrics,
+        .collect     = num_tasks_collect,
+    };
+    prom_register(registry, num_tasks_col);
 }
